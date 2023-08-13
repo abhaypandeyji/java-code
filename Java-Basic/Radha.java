6c08d29805428a073ec4108dbd94b636b37bb114
Abhay Kumar Pandey <137700321+abhaypandeyji@users.noreply.github.com>
2023-08-13 16:33:19 +0000

    container config done

--- Java-Basic/Radha.java
+++ Java-Basic/Radha.java
@@ -1,8 +1,15 @@
+import java.util.*;
 class Radha{
     public static void main(String[] args) {
         int a = 12;
         int b = 13;
         System.out.println(a+b);
         System.out.println("Jai shree Krisha bolo jai Radhe");
+        LinkedHashSet<Character> hs = new LinkedHashSet<>();
+        String str = "apple";
+        for(int i = 0;i<str.length();i++){
+            hs.add(str.charAt(i));
+        }
+        System.out.println(hs);
     }
 }
