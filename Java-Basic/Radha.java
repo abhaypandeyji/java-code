7af2f70028c2ba9a3b243c40b2ff920cc328bca4
Abhay Kumar Pandey <137700321+abhaypandeyji@users.noreply.github.com>
2023-08-13 16:26:45 +0000

    container config done

---
+++ Java-Basic/Radha.java
@@ -0,0 +1,8 @@
+class Radha{
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 13;
+        System.out.println(a+b);
+        System.out.println("Jai shree Krisha bolo jai Radhe");
+    }
+}
